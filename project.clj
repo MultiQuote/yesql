970cd5d8f40ca55632487659c550694dcf0ead36
CI <>
2021-08-09 12:59:01 +0000

    Version 0.5.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yesql "0.5.4-SNAPSHOT"
+(defproject yesql "0.5.4"
   :description "A Clojure library for using SQL (forked)"
   :url "https://github.com/krisajenkins/yesql"
   :license {:name "Eclipse Public License"
